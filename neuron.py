d95abb2ad1af66946145c488f282421f97475bd1
Balint Vekerdy <balint@dangerfarms.com>
2016-02-16 21:36:43 +0000

    fix

--- neuron.py
+++ neuron.py
@@ -5,7 +5,7 @@ class Neuron(object):
         self.threshold = threshold
 
     def get_output(self, inputs):
-        return inputs
+        return [self.weights[i] * inp for i, inp in enumerate(inputs)]
 
 
 class Layer(object):
@@ -16,12 +16,17 @@ class Layer(object):
     def feed(self, inputs):
         outputs = []
         for neuron in self.neurons:
-            outputs.append(neuron.get_ouput(inputs))
+            outputs.append(neuron.get_output(inputs))
         self.outputs = outputs
         return outputs
 
     def __repr__(self):
-        template = ('{} | ' * len(self.neurons)).strip()
-        import ipdb; ipdb.set_trace()
-        return template.format(*[o for o in self.outputs])
+        return ' | '.join([str(o) for o in self.outputs])
 
+
+def create_dummy_layer():
+    n1 = Neuron(num_inputs=2, weights=[.75, .25], threshold=0.5)
+    n2 = Neuron(num_inputs=2, weights=[.05, .95], threshold=1)
+    l1 = Layer([n1, n2])
+    l1.feed([1, 2])
+    print(l1)
